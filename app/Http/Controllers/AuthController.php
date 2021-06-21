0f8764302a3e5f14dbe484c271cd8227fd271e57
hellstrike12 <hellstrike12@github.com>
2021-06-21 08:06:48 -0300

    Created AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', [ 'except' => ['login'] ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = auth()->attempt($credentials) ) {
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function user()
+    {
+        return reponse()->json( auth()->user() );
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken( auth()->refresh() );
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return reponse()->json([
+            'success' => true,
+            'message' => 'User successfully logged out'
+        ]);
+    }
+
+    public function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ], 200);
+    }
+}
